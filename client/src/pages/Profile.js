7f54be5e7d8a310b1258fd5b4390b1287a3e36d7
Greg <gknudsen@duqlight.com>
2021-04-06 14:21:27 -0400

    add reaction mutation

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 
+
 import { ADD_FRIEND } from "../utils/mutations";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
